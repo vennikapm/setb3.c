94f4aab5310c0f5e5f9874a7c15105b96ee568bd
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-27 00:04:38 -0800

    Create set3.6.c

---
+++ set3.6.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,num,a[100],k=0,i,temp,j;
+clrscr();
+printf("enter test");
+scanf("%d",&n);
+while(n>0)
+{
+printf("enter arr");
+scanf("%d",&num);
+for(i=0;i<num;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<num;i++)
+{
+for(j=i+1;j<num;j++)
+{
+if(a[j]<a[i])
+{
+    temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+}
+}
+}
+for(i=0;i<num;i++)
+{
+printf("%d",a[i]);
+}
+n--;
+}
+getch();
+}
