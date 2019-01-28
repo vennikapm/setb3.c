4f99528214e9e44a8093fa8a7be778156c5955da
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-28 07:42:06 -0800

    Create set4.2.c

---
+++ set4.2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,count=0;
+char a[100];
+clrscr();
+scanf("%[^\n]s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("%d",count+1);
+getch();
+}
