7f51214b5c42c796b5a8429dcd7a350599a85638
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-28 09:01:58 -0800

    Create set4.9.c

---
+++ set4.9.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],i,max,n;
+clrscr();
+scanf("%d",&n);
+if(n==10)
+{
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=1;i<n;i++)
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+}
+printf("%d",max);
+}
+else
+{
+printf("not poss");
+}
+getch();
+}
