d7482dcf5c546c6bd6aaf2da0e6cac063019f29b
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-01-28 09:58:03 -0800

    Create set5.4.c

---
+++ set5.4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,a=1,b=10;
+clrscr();
+scanf("%d",&num);
+if(num>=a&&num<=b)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
