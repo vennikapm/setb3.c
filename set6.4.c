cdce988341675ddbd7037b9dfcf50fcd26266b85
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-01 22:16:07 -0800

    Create set6.4.c

---
+++ set6.4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,num;
+clrscr();
+scanf("%d",&n);
+num=n-1;
+if(num%2==0)
+{
+printf("%d",num);
+}
+else
+{
+printf("%d",n);
+}
+getch();
+}
