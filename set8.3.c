c0046e05040227472348a5c79e0ea6123b64760c
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-08 06:30:42 -0800

    Create set8.3.c

---
+++ set8.3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,num;
+clrscr();
+scanf("%d",&num);
+scanf("%d%d",&a,&b);
+if(num>=a&&num<=b)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
+
