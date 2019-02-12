4e0f24388fe70537562de2f02f112b1ed83c9b11
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-12 06:59:02 -0800

    Create set9.1.c

---
+++ set9.1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,sub;
+    clrscr();
+    scanf("%d %d",&a,&b);
+    sub=a-b;
+    if(sub>0)
+    {
+        sub=sub;
+    }
+    else
+    {
+        sub=-(sub);
+    }
+    printf("%d",sub);
+getch();
+}
