8264d7f6b20bc0310d0d54431d0d5603bdfd4ed4
vennikapm <46838407+vennikapm@users.noreply.github.com>
2019-02-17 02:58:14 -0800

    Create set 10.6.c

---
+++ set 10.6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main()
+{
+     int a, d, n, i, tn;
+     int sum = 0;
+ 
+    
+     scanf("%d", &a);
+     scanf("%d", &d);
+     scanf("%d", &n);
+     sum = (n * (2 * a + (n - 1)* d ))/ 2;
+     tn = a + (n - 1) * d;
+     for (i = a; i <= tn; i = i + d )
+     {
+        
+               
+               printf("%d ", sum);
+               break;
+     }
+    
+     return 0;
+}
